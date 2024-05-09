dd8ed3f2bb8f2e4d23b984ca0785221ec5126dd7
sreedharanbraincade <sridhardigital22@gmail.com>
2024-05-09 08:03:14 +0530

    initial commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+const SignUpPgae = () => {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+};
+
+export default SignUpPgae;
