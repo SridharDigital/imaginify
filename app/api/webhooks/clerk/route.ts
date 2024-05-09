3da1ec1e6eb6b0c7bcb0efeb2382d7832e88ce36
sreedharanbraincade <sridhardigital22@gmail.com>
2024-05-09 09:16:15 +0530

    moved route.ts of clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -61,6 +61,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
+    console.log("User.created is executed");
 
     const user = {
       clerkId: id,
